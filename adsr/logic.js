92024fc16e00c4c390f1ba6c1ac04d7016827ab5
andrewshallvr <connor.c.hudson@colorado.edu>
2017-01-30 17:03:11 -0700

    moar progress

--- adsr/logic.js
+++ adsr/logic.js
@@ -2,6 +2,6 @@ var scene = document.querySelector('a-scene');
 var synth = new Tone.Synth().toMaster();
 var loop = new Tone.Sequence(function(time, index){
       //iterate over things here
-		}, [0, 1, 2, 3, 4, 5, 6, 7], "8n");
+      //synth.triggerAttackRelease("C4", "16n", time="+8n");
+		}, [0, 1, 2, 3], "4n");
 Tone.Transport.start();
-loop.start();
